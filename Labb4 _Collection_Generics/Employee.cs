a9449347e563d3cee5d4a5d2710c079ded627ca8
NAEL SHARABI <nael_sps@hotmail.com>
2021-10-20 13:39:03 +0200

    Initial commit

---
+++ Labb4 _Collection_Generics/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace Labb4__Collection_Generics
+{
+    class Employee
+    {
+        protected string ID { get; set; } = "0000";
+        protected string Name { get; set; }
+        protected string Gender { get; set; }
+        protected double Salary { get; set; }
+        public Employee(string id, string name, string gender, int salary)
+        {
+            ID = id;
+            Name = name;
+            Gender = gender;
+            Salary = salary;
+        }
+        public override string ToString()
+        {
+            return $"\n\tID: {ID}\n\tFull name: {Name}" +
+                $"\n\tGender: {Gender}\n\tSalary: {Salary}\n" +
+                $"___________________________________________________________";
+        }
+    }
+}
---
+++ Labb4 _Collection_Generics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace Labb4__Collection_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee_1 = new Employee("0001", "Nael Sharabi", "Male", 30000);
+            Employee employee_2 = new Employee("0002", "Gillian Brown", "Female", 25000);
+            Employee employee_3 = new Employee("0003", "Robin Svensson", "Male", 35000);
+            Employee employee_4 = new Employee("0004", "Hans Steiner", "Male", 29000);
+            Employee employee_5 = new Employee("0005", "Mike Jefferson", "Male", 23000);
+
+            Stack stack = new Stack();
+            stack.Push(employee_1);
+            stack.Push(employee_2);
+            stack.Push(employee_3);
+            stack.Push(employee_4);
+            stack.Push(employee_5);
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
